11af35e7d933c9dfb956f68dcc4684c242d94fc1
marcebenitez2 <marcebenitez0607@gmail.com>
2024-06-25 11:21:32 -0300

    feat: ahora se ve la descripcion en caso que la tenga el producto

--- src/modules/product/components/DescriptionProduct.tsx
+++ src/modules/product/components/DescriptionProduct.tsx
@@ -10,12 +10,10 @@ export const DescriptionProduct = ({ product }: { product: IProducts }) => {
     <section className="w-full">
       <h3 className="text-3xl">{product.attributes.title}</h3>
       {/* Descripcion en caso que haya */}
-      <p className="text-base">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque deleniti
-        atque molestiae quas ullam ea dignissimos recusandae ad fugit vero
-        quibusdam, voluptatum ipsam illo vel dolores nesciunt aliquid dolorem
-        id.
-      </p>
+
+      {product.attributes.description ? (
+        <p className="text-base">{product.attributes.description}</p>
+      ) : null}
 
       {/* Precio */}
       <p className="text-lg font-bold">${product.attributes.price}</p>
